39ac636df0017a7f45a32542e33f6c32972dd90f
amirz <amirzmollataghiii@gmail.com>
2026-06-17 16:09:52 +0330

    Add custom form validation

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Contact
+from datetime import date
 
 class ContactForm(forms.ModelForm):
     
@@ -7,6 +8,7 @@ class ContactForm(forms.ModelForm):
         required=False,
         label = "تصویر"
     )
+    
     class Meta:
         
         model = Contact
@@ -15,3 +17,27 @@ class ContactForm(forms.ModelForm):
             "mobile",
             "birth_date",
         ]
+        
+    def clean_birth_date(self):
+        
+        birth_date = self.cleaned_data.get("birth_date")
+        
+        if birth_date and birth_date > date.today():
+            
+            raise forms.ValidationError(
+                "تاریخ نمی تونه در آینده باشه!"
+            )
+            
+        return birth_date
+    
+    def clean_full_name(self):
+        
+        full_name = self.cleaned_data.get("full_name")
+        
+        if len(full_name.strip()) < 3:
+            
+            raise forms.ValidationError(
+                "نام باید حداقل 3 کاراکتر باشه!"
+            )
+            
+        return full_name
