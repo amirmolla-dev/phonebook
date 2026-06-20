38fd842149df70ab11ae913deff244c101e0d751
amirz <amirzmollataghiii@gmail.com>
2026-06-20 16:14:22 +0330

    Migrate project from sqlite to MySQL

--- phonebook/settings.py
+++ phonebook/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'phonebook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "phonebook_db",
+        "USER": "phonebook_user",
+        "PASSWORD": "PhoneBook@123",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
